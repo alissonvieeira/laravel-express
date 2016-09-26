d1253d39777fe92fd0d64708ec781d65f6eb54dd
alissonvieeira <alisson.echo@gmail.com>
2016-09-25 22:22:27 -0300

    Removendo e linkando

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,7 +2,10 @@
 
 @section('content')
 	<h1>Blog Admin</h1>
-
+	
+	<a href="{{ route('admin.posts.create') }}" class="btn btn-success">Create new post</a>
+	<br>
+	<br>
 
 	<table class="table">
 		<tr>
@@ -15,7 +18,10 @@
 		<tr>
 			<td>{{ $post->id }}</td>
 			<td>{{ $post->title }}</td>
-			<td></td>
+			<td>
+				<a href="{{ route('admin.posts.edit', ['id' => $post->id]) }}" class="btn btn-default">Edit</a>
+				<a href="{{ route('admin.posts.destroy', ['id' => $post->id]) }}" class="btn btn-danger">Destroy</a>
+			</td>
 		</tr>
 		@endforeach
 	</table>
