db03741842eff5ab757897593d20b7ac5767247e
alissonvieeira <alisson.echo@gmail.com>
2016-09-29 14:05:32 -0300

    Alterando banco

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,7 +32,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
 
 	Route::group(['prefix' => 'posts'], function(){
 
-		Route::get('', ['as' => 'admin.posts.index', 'uses' => 'PostsAdminController@index']);
+		Route::get('/', ['as' => 'admin.posts.index', 'uses' => 'PostsAdminController@index']);
 		Route::get('create', ['as' => 'admin.posts.create', 'uses' => 'PostsAdminController@create']);
 		Route::post('store', ['as' => 'admin.posts.store', 'uses' => 'PostsAdminController@store']);
 		Route::get('edit/{id}', ['as' => 'admin.posts.edit', 'uses' => 'PostsAdminController@edit']);
